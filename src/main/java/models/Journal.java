fa8ce4bb8ed138c0786e8ac6c4af8f493e64728b
Tenae <tenae@zipcodersmbp107.myfiosgateway.com>
2022-07-19 22:30:32 -0400

    added fields

--- src/main/java/models/Journal.java
+++ src/main/java/models/Journal.java
@@ -1,4 +1,11 @@
 package models;
 
 public class Journal {
+    private int id;
+    private String type;
+    private String brand;
+    private int size;
+    private int qty;
+    private float price;
 }
+
--- src/main/java/models/Planner.java
+++ src/main/java/models/Planner.java
@@ -1,4 +1,10 @@
 package models;
 
 public class Planner {
+    private int id;
+    private String type;
+    private String brand;
+    private int size;
+    private int qty;
+    private float price;
 }
