6227376e8b13a31f8baa73fbe6662a9f7cd0f232
Tenae <tenae@Zipcoders-MacBook-Pro-107.local>
2022-07-20 16:22:56 -0400

    started section 2 part 3

--- src/main/java/models/Journal.java
+++ src/main/java/models/Journal.java
@@ -6,6 +6,60 @@ public class Journal {
     private String brand;
     private int size;
     private int qty;
-    private float price;
+    private double price;
+
+    public Journal(){}
+    public Journal (int id, String type, String brand, int size, int qty, double price){
+        this.id = id;
+        this.type = type;
+        this.brand = brand;
+        this.size = size;
+        this.qty = qty;
+        this.price = price;
+
+    }
+
+
+    public void setBrand(String brand ) {
+        this.brand = brand;
+    }
+
+    public String getBrand() { return brand;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setType(String type){
+        this.type = type;
+    }
+    public String getType() {
+        return type;
+    }
+    public void setSize(int size){
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+    public int getQty() {
+        return qty;
+    }
+
+    public void setPrice(){
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
 
--- src/test/java/modelsTest/JournalTest.java
+++ src/test/java/modelsTest/JournalTest.java
@@ -1,4 +1,58 @@
 package modelsTest;
 
+import models.Journal;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class JournalTest {
+    @Test
+    public void setBrandTest() {
+        // given
+        String expectedBrand = "Erin Condren";
+
+        // when
+        Journal newJournal = new Journal();
+        newJournal.setBrand(expectedBrand);
+
+        // then
+        Assertions.assertEquals(expectedBrand, newJournal.getBrand());
+    }
+
+    @Test
+    public void setIdTest(){
+        //given
+        int expectedId = 2;
+
+        //when
+        Journal testJournal = new Journal();
+        testJournal.setId(expectedId);
+
+        //then
+        Assertions.assertEquals(expectedId, testJournal.getId());
+
+    }
+
+    @Test
+    public void constructorTest() {
+        //given
+        int expectedId = 6;
+        String expectedType = "Weekly";
+        String expectedBrand = "Happy Planner";
+        int expectedSize = 8;
+        int expectedQty = 1000;
+        double expectedPrice = 35.53;
+
+        //when
+      Journal testJournal  = new Journal (expectedId, expectedType, expectedBrand, expectedSize, expectedQty, expectedPrice);
+
+
+        //then
+        Assertions.assertEquals(expectedId, testJournal.getId());
+        Assertions.assertEquals(expectedType, testJournal.getType());
+        Assertions.assertEquals(expectedBrand, testJournal.getBrand());
+        Assertions.assertEquals(expectedSize, testJournal.getSize());
+        Assertions.assertEquals(expectedQty, testJournal.getQty());
+        Assertions.assertEquals(expectedPrice, testJournal.getPrice());
+
+    }
 }
